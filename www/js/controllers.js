f3c6939c8bf9a7d279c68e25eee47212e72c5d1e
Yohanes Gultom <yohanes.gultom@gmail.com>
2015-08-22 22:55:59 +0800

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,232 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+// .controller('DashCtrl', function($scope) {})
+//
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   // With the new view caching in Ionic, Controllers are only called
+//   // when they are recreated or on app start, instead of every page change.
+//   // To listen for when this page is active (for example, to refresh data),
+//   // listen for the $ionicView.enter event:
+//   //
+//   //$scope.$on('$ionicView.enter', function(e) {
+//   //});
+//
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   };
+// })
+//
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
+
+.controller('AccountCtrl', function($scope, $ionicModal, Commons) {
+  $scope.login = {};
+  $scope.config = {
+    SMSServer: Commons.SMSServer(),
+    APIServer: Commons.APIServer()
+  };
+
+  $scope.$on('$ionicView.beforeLeave', function() {
+    if ($scope.config.SMSServer != Commons.SMSServer() || $scope.config.APIServer != Commons.APIServer()) {
+      if (confirm('Simpan konfigurasi?')) {
+        Commons.updateSMSServer($scope.config.SMSserver);
+        Commons.updateAPIServer($scope.config.APIServer);
+      }
+    }
+  });
+
+  $ionicModal.fromTemplateUrl('login-modal.html', {
+   scope: $scope,
+   animation: 'slide-in-up'
+  }).then(function(modal) {
+   $scope.modal = modal
+  })
+
+ $scope.openModal = function() {
+   $scope.modal.show()
+ }
+
+ $scope.closeModal = function() {
+   $scope.modal.hide();
+ };
+
+ $scope.$on('$destroy', function() {
+   $scope.modal.remove();
+ });
+ 
+})
+
+/* Cek Harga! */
+
+.controller('CekHargaCtrl', function($scope, $ionicLoading, $cordovaSms, $ionicPlatform, Commons, Webservice) {
+
+  // set options
+  $scope.items = Commons.items();
+  $scope.sellers = Commons.sellers();
+  $scope.selection = {
+    item: ($scope.items.length > 0) ? $scope.items[0].code : null,
+    seller: ($scope.sellers.length > 0) ? $scope.sellers[0].code : null
+  };
+  $scope.currentPosition = null;
+
+  $scope.onPositionFound = function(position) {
+    $scope.currentPosition = position;
+    var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: $scope.map,
+      title: 'Posisi Anda (' + position.coords.latitude + ', ' +  position.coords.longitude + ')'
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: marker.title
+    }).open($scope.map, marker);
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open($scope.map,marker);
+    });
+    $scope.map.setCenter(myLatlng);
+  }
+
+  $scope.onPositionError = function(err) {
+    alert('Lokasi tidak berhasil dideteksi : ' + err.message + ' (' + err.code + ')');
+  }
+
+  $scope.getUserCurrentLocation = function(options) {
+    navigator.geolocation.getCurrentPosition($scope.onPositionFound,
+      function(err) {
+        options.enableHighAccuracy = true; // force to use GPS
+        navigator.geolocation.getCurrentPosition($scope.onPositionFound, $scope.onPositionError, options);
+      },
+      options);
+    }
+
+    $scope.init = function() {
+      $scope.map = new google.maps.Map(document.getElementById("map"), {zoom: 16,mapTypeId: google.maps.MapTypeId.ROADMAP});
+      $scope.getUserCurrentLocation(
+        {maximumAge: 30000, timeout: 20000, enableHighAccuracy: false}
+      );
+    }
+
+    // in case position was not found, try it again on view re-enter
+    $scope.$on('$ionicView.enter', function() {
+      if ($scope.map && !$scope.currentPosition) {
+        $scope.init();
+      }
+    });
+
+    $scope.getPrice = function(source) {
+      if ($scope.selection.item && $scope.selection.seller) {
+        $ionicLoading.show();
+        if ('WEB' == source) {
+          Webservice.example(
+            {barang:$scope.selection.item, penjual:$scope.selection.seller},
+            function(res) {
+              alert(JSON.stringify(res));
+              $ionicLoading.hide();
+            },
+            function(err) {
+              alert(err);
+              $ionicLoading.hide();
+            }
+          );
+        } else if ('SMS' == source) {
+          var phonenumber = Commons.SMSServer();
+          var content = 'CEKHARGA ' + $scope.selection.item.toUpperCase() + ' ' + $scope.selection.seller.toUpperCase();
+          alert(content);
+          try {
+            $ionicPlatform.ready(function() {
+              $cordovaSms
+              .send(phonenumber, content)
+              .then(function() {
+                alert('SMS berhasil dikirim')
+                $ionicLoading.hide();
+              }, function(error) {
+                alert('SMS gagal dikirim: ' + error);
+                $ionicLoading.hide();
+              });
+            });
+          } catch(err) {
+            alert(err);
+            $ionicLoading.hide();
+          }
+        }
+      }
+    }
+
+  })
+
+  .controller('PosHargaCtrl', function($scope, $ionicLoading, $ionicPlatform, Commons, Webservice) {
+    // set options
+    $scope.items = Commons.items();
+    $scope.sellers = Commons.sellers();
+    $scope.selection = {
+      item: ($scope.items.length > 0) ? $scope.items[0].code : null,
+      seller: ($scope.sellers.length > 0) ? $scope.sellers[0].code : null,
+      price: null
+    };
+    $scope.source = null;
+
+    $scope.sendPrice = function(source) {
+      if (source && $scope.selection.item && $scope.selection.seller && $scope.selection.item) {
+        $scope.source = source;
+        $ionicLoading.show();
+        var options = {maximumAge: 30000, timeout: 20000, enableHighAccuracy: false};
+        navigator.geolocation.getCurrentPosition(
+          $scope.onPositionFound,
+          function(err) {
+            options.enableHighAccuracy = true; // force native GPS
+            navigator.geolocation.getCurrentPosition(
+              $scope.onPositionFound,
+              $scope.onPositionError,
+              options
+            );
+          },
+          options);
+        }
+      }
+
+      $scope.onPositionFound = function(pos) {
+        if ('WEB' == $scope.source) {
+          Webservice.example(
+            {barang: $scope.selection.item, penjual: $scope.selection.seller, harga: $scope.selection.price, lat: pos.coords.latitude, lng: pos.coords.longitude},
+            function(res) {
+              alert(JSON.stringify(res));
+              $ionicLoading.hide();
+            },
+            function(err) {
+              alert(err);
+              $ionicLoading.hide();
+            }
+          );
+        } else if ('SMS' == $scope.source) {
+          var phonenumber = Commons.SMSServer();
+          var content = 'POSHARGA ' + $scope.selection.item.toUpperCase() + ' ' + $scope.selection.seller.toUpperCase() + ' ' + $scope.selection.price + ' ' + pos.coords.latitude + ' ' + pos.coords.longitude;
+          alert(content);
+          $ionicLoading.show();
+          try {
+            $ionicPlatform.ready(function() {
+              $cordovaSms
+              .send(phonenumber, content)
+              .then(function() {
+                alert('SMS berhasil dikirim')
+                $ionicLoading.hide();
+              }, function(error) {
+                alert('SMS gagal dikirim: ' + error);
+                $ionicLoading.hide();
+              });
+            });
+          } catch(err) {
+            alert(err);
+            $ionicLoading.hide();
+          }
+        }
+      }
+
+      $scope.onPositionError = function(err) {
+        alert('Lokasi tidak berhasil dideteksi : ' + err.message + ' (' + err.code + ')');
+        $ionicLoading.hide();
+      }
+
+    })
+    ;
---
+++ www/js/services.js
@@ -0,0 +1,124 @@
+angular.module('starter.services', [])
+
+.factory('Commons', function() {
+
+  var _sms_server = '+14077925761';
+  var _api_server = 'http://crosscode.tk/dimana/api';
+
+  var _items = [
+    {code: 'BAWANG_MERAH', name: 'Bawang merah'},
+    {code: 'PISANG_AMBON', name: 'Pisang ambon'},
+    {code: 'APEL_MALANG', name: 'Apel malang'}
+  ]
+
+  var _sellers = [
+    {code: 'PASAR_001', name: 'Pasar 001'},
+    {code: 'SUPERMARKET_002', name: 'Supermarket 002'},
+    {code: 'TOKO_003', name: 'Toko 003'}
+  ]
+
+  return {
+    items: function() {
+      return _items;
+    },
+    sellers: function() {
+      return _sellers;
+    },
+    SMSServer: function() {
+      return _sms_server;
+    },
+    APIServer: function() {
+      return _api_server;
+    },
+    updateSMSServer: function(newVal) {
+      if (_sms_server != newVal) {
+        _sms_server = newVal;
+        return true;
+      }
+      return false;
+    },
+    updateAPIServer: function(newVal) {
+      if (_api_server != newVal) {
+        _api_server = newVal;
+        return true;
+      }
+      return false;
+    }
+  }
+
+})
+
+.service('Webservice', function($http, Commons) {
+
+  var me = this;
+
+  this.urlencode = function(obj) {
+      var str = [];
+      for(var p in obj) str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      return str.join("&");
+  }
+
+  this.example = function(data, onSuccess, onError) {
+    $http({
+      method: 'POST',
+      url: Commons.APIServer() + '/example',
+      data: me.urlencode(data),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    .success(onSuccess)
+    .error(onError);
+  }
+})
+
+// .factory('Chats', function() {
+//   // Might use a resource here that returns a JSON array
+//
+//   // Some fake testing data
+//   var chats = [{
+//     id: 0,
+//     name: 'Ben Sparrow',
+//     lastText: 'You on your way?',
+//     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+//   }, {
+//     id: 1,
+//     name: 'Max Lynx',
+//     lastText: 'Hey, it\'s me',
+//     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+//   }, {
+//     id: 2,
+//     name: 'Adam Bradleyson',
+//     lastText: 'I should buy a boat',
+//     face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+//   }, {
+//     id: 3,
+//     name: 'Perry Governor',
+//     lastText: 'Look at my mukluks!',
+//     face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+//   }, {
+//     id: 4,
+//     name: 'Mike Harrington',
+//     lastText: 'This is wicked good ice cream.',
+//     face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+//   }];
+//
+//   return {
+//     all: function() {
+//       return chats;
+//     },
+//     remove: function(chat) {
+//       chats.splice(chats.indexOf(chat), 1);
+//     },
+//     get: function(chatId) {
+//       for (var i = 0; i < chats.length; i++) {
+//         if (chats[i].id === parseInt(chatId)) {
+//           return chats[i];
+//         }
+//       }
+//       return null;
+//     }
+//   };
+// })
+
+;
