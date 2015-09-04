379cb588c5c331c1d081c7b12697674a134b65ce
Yohanes Gultom <yohanes.gultom@gmail.com>
2015-09-04 08:43:21 +0700

    update release script and apk

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-rm pantauharga.apk ||
+rm dist/pantauharga.apk ||
 cordova build --release android &&
 /c/Program\ Files/Java/jdk1.7.0_67/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name &&
 /c/Android/sdk/build-tools/22.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk dist/pantauharga.apk
